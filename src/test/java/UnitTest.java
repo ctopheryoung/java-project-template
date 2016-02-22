8f70fc561481e66095810da30f2b91ec4de73720
Chris Young <ctopheryoung@gmail.com>
2016-02-21 19:43:41 -0800

    add database rule

--- src/test/java/UnitTest.java
+++ src/test/java/UnitTest.java
@@ -1,9 +1,10 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.junit.Rule;
 
 public class UnitTest {
   public DatabaseRule database = new DatabaseRule();
-  
+
   //Tests go here
 
 }
